0a33fce62f5963cb9aca743c2647755012a6dcbf
ahmedshabaanhelwa <ahmedshabaan@outlook.com>
2022-05-07 20:46:10 +0200

    Feature 2: Move Forward. Cycle 3: Refactor.

--- MarsRover/MarsRover.cs
+++ MarsRover/MarsRover.cs
@@ -5,8 +5,20 @@ public class MarsRover
     private string _InittialState;
     public MarsRover(string initialState) => _InittialState = initialState;
 
-    public string Execute(string commands)
+    public string Execute(string command)
     {
+        // Parsing the command
+        string[] commands = command.Split(':');
+        int x = int.Parse(commands[0]);
+        int y = int.Parse(commands[1]);
+        int direction = int.Parse(commands[2]);
+
+        // Moving in Y-Axis. Forward and backward
+        if(command == "M")
+        {
+            return $"{x}:{y+1}:{direction}";
+        }
+
         return _InittialState;
     }
 }
