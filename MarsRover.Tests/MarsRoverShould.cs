98e0cb8a5c5ad1173acf975a50c4a499cdd9e894
ahmedshabaanhelwa <ahmedshabaan@outlook.com>
2022-05-07 17:50:35 +0200

    Add MarsRoverShould testing class with PrintInitialState method.

---
+++ MarsRover.Tests/MarsRoverShould.cs
@@ -0,0 +1,33 @@
+using FluentAssertions;
+using Xunit;
+
+namespace MarsRover.Tests;
+/// <remarks>
+/// classes names follow Should/ShouldNot convention
+/// </remarks>
+public class MarsRoverShould
+{
+    [Fact]
+    public void PrintInitialState()
+    {
+        // Arrange - (G)iven - To arrange your setup for the test
+        var marsRover = new MarsRover(initialState: "0:0:N");
+        // Act - (W)hen - To Act on your unit
+        var result = marsRover.Execute(commands: "");
+        // Assert - (T)hat - To verify your test.
+        result.Should().Be("0:0:N");
+    }
+
+    public class MarsRover
+    {
+        public MarsRover(string initialState)
+        {
+
+        }
+
+        internal string Execute(string commands)
+        {
+            return "0:0:N";
+        }
+    }
+}
