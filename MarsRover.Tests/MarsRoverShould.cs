35603eedbee6f64b721a48b32686da285188f09e
ahmedshabaanhelwa <ahmedshabaan@outlook.com>
2022-05-07 20:04:58 +0200

    Tringulation Cycle: Make tests for centeric and non-centeric positions.

--- MarsRover.Tests/MarsRoverShould.cs
+++ MarsRover.Tests/MarsRoverShould.cs
@@ -8,7 +8,7 @@ namespace MarsRover.Tests;
 public class MarsRoverShould
 {
     [Fact]
-    public void PrintInitialState()
+    public void PrintInitialStateFromCenterPosition()
     {
         // Arrange - (G)iven - To arrange your setup for the test
         var marsRover = new MarsRover(initialState: "0:0:N");
@@ -16,5 +16,15 @@ public class MarsRoverShould
         var result = marsRover.Execute(commands: "");
         // Assert - (T)hat - To verify your test.
         result.Should().Be("0:0:N");
+    }
+    [Fact]
+    public void PrintInitialStateFromNoneCentericPosition()
+    {
+        // Arrange - (G)iven - To arrange your setup for the test
+        var marsRover = new MarsRover(initialState: "4:2:W");
+        // Act - (W)hen - To Act on your unit
+        var result = marsRover.Execute(commands: "");
+        // Assert - (T)hat - To verify your test.
+        result.Should().Be("4:2:W");
     }
 }
