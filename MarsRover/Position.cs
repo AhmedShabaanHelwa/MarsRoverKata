bd327785e0f616ed8d9635a27c735e5df9b0984c
ahmedshabaanhelwa <ahmedshabaan@outlook.com>
2022-05-09 13:16:47 +0200

    Feature 2: Move Forward. Cycle 3: Refactor. Move Position class to a separate file.

---
+++ MarsRover/Position.cs
@@ -0,0 +1,16 @@
+﻿namespace MarsRover;
+
+/// <summary>
+/// Value object to hold position of the rover
+/// </summary>
+internal class Position
+{
+    internal int _x;
+    internal int _y;
+    // Default constructor
+    public Position() 
+    { 
+        
+    }
+    public Position(int x, int y) => (this._x, this._y) = (x, y);
+}
