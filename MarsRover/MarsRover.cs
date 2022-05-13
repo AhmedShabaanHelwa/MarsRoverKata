0e65cacbe5946340904f04777f67167b48ed5af6
ahmedshabaanhelwa <ahmedshabaan@outlook.com>
2022-05-13 19:07:40 +0200

    Refactoring Cycle of Feature 1: Print initial state Move MarsRover class to separate class in MarsRover project.

---
+++ MarsRover/MarsRover.cs
@@ -0,0 +1,14 @@
+﻿namespace MarsRover;
+
+public class MarsRover
+{
+    public MarsRover(string initialPosition)
+    {
+        
+    }
+
+    public string Execute(string commands)
+    {
+        return "0:0:N";
+    }
+}
