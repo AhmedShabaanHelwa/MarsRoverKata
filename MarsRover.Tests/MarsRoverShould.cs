22c66cfe2a56cd146f0aa2bec4be2d65a6f7ab7f
ahmedshabaanhelwa <ahmedshabaan@outlook.com>
2022-05-14 17:51:40 +0200

    Feature 2: Move Forward. Mini cycle: Triangulating move forward to include all directions: N, E, W, and S.

--- MarsRover.Tests/MarsRoverShould.cs
+++ MarsRover.Tests/MarsRoverShould.cs
@@ -19,14 +19,18 @@ public class MarsRoverShould
     }
 
     [Theory]
-    [InlineData("0:0:N")]
-    internal void MoveForward(string initialPosition)
+    [InlineData("0:0:N", "0:1:N")] // North - From center
+    [InlineData("5:2:N", "5:3:N")] // North - From non-center position
+    [InlineData("7:3:E", "8:3:E")] // East
+    [InlineData("9:5:W", "8:5:W")] // West
+    [InlineData("2:6:S", "2:5:S")] // South
+    internal void MoveForward(string initialPosition, string finalPosition)
     {
         // Arrange
         MarsRover marsRover = new(initialPosition);
         // Act
         string result = marsRover.Execute("M");
         // Assert
-        result.Should().Be("0:1:N");
+        result.Should().Be(finalPosition);
     }
 }
