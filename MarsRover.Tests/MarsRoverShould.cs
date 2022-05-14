d304bc6dbffca5b83a3933d62743c6fe4fa3b248
ahmedshabaanhelwa <ahmedshabaan@outlook.com>
2022-05-14 16:43:02 +0200

    Feature 1: Print Initial State. Cycle3: Refactor. Position triangulation

---
+++ MarsRover.Tests/MarsRoverShould.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace MarsRover.Tests;
+
+public class MarsRoverShould
+{
+    [Theory]
+    [InlineData("0:0:N")] // Centric position
+    [InlineData("6:4:W")] //
+    internal void PrintInitialState(string initialState)
+    {
+        // Arrange
+        MarsRover marsRover = new MarsRover(initialState);
+        // Act
+        var result = marsRover.Execute("");
+        // Assert
+        result.Should().Be(initialState);
+    }
+}
