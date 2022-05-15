d45f8be6602f39aa7f4ae768d2822d6c40da783a
ahmedshabaanhelwa <ahmedshabaan@outlook.com>
2022-05-15 22:58:44 +0200

    Feature 3: Turn Right. Cycle 3: Refactor. Refactor MarsRover.Execute() method to change the direction to right.

--- MarsRover/MarsRover.cs
+++ MarsRover/MarsRover.cs
@@ -7,6 +7,7 @@ public class MarsRover
     private readonly string _initialState;
     private IDirection? _direction;
     private Position? _position;
+
     public MarsRover(string initialState)
     {
         _initialState = initialState;
@@ -19,7 +20,7 @@ public class MarsRover
         int x = int.Parse(states[0]);
         int y = int.Parse(states[1]);
         string directionStringCommand = states[2];
-        _position = new Position(x,y);
+        _position = new Position(x, y);
         _direction = DirectionFactory.CreateFrom(directionStringCommand);
         // Forward
         if (commands == "M")
@@ -28,14 +29,7 @@ public class MarsRover
         }
         else if (commands == "R")
         {
-            if (directionStringCommand == "N")
-                _direction = _direction.ToRight();
-            else if (directionStringCommand == "E")
-                _direction = _direction.ToRight();
-            else if (directionStringCommand == "S")
-                _direction = _direction.ToRight();
-            else if (directionStringCommand == "W")
-                _direction = _direction.ToRight();
+            _direction = _direction.ToRight();
         }
 
         return $"{_position.X}:{_position.Y}:{_direction.AsStringCommand()}";
