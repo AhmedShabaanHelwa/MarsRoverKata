5f2e32c7a81cef38202e4f6e5444252f95064b68
ahmedshabaanhelwa <ahmedshabaan@outlook.com>
2022-05-16 10:43:31 +0200

    Feature 5: Multiple Commands. Cycle 3: Refactor. Solve code smell of switch statement (Lack of polymorphism) by introducing MoveForward(), TurnRight() and TurnLeft() methods in Rover class, and modify Rover fields to be read only.

--- MarsRover/MarsRover.cs
+++ MarsRover/MarsRover.cs
@@ -4,38 +4,26 @@ namespace MarsRover;
 
 internal class Rover
 {
-    internal IDirection? _direction;
-    internal Position? _position;
+    internal readonly IDirection? _direction;
+    internal readonly Position? _position;
 
-    public Rover()
-    {
-    }
+    public Rover(Position position, IDirection direction) => (_position, _direction) = (position,direction);
 
-    public void TurnLeft()
-    {
-        this._direction = this._direction.ToLeft();
-    }
+    public Rover TurnLeft() => new Rover(_position, _direction.ToLeft());
 
-    public void MoveForward()
-    {
-        this._position = this._direction.MoveForward(this._position);
-    }
+    public Rover MoveForward() => new Rover(_direction.MoveForward(_position), _direction);
 
-    public void TurnRight()
-    {
-        this._direction = this._direction.ToRight();
-    }
+    public Rover TurnRight() => new Rover(_position, _direction.ToRight());
 }
 
 public class MarsRover
 {
     private readonly string _initialState;
-    private readonly Rover _rover;
+    private Rover _rover;
 
     public MarsRover(string initialState)
     {
         _initialState = initialState;
-        _rover = new Rover();
     }
 
     public string Execute(string commands)
@@ -45,21 +33,22 @@ public class MarsRover
         int x = int.Parse(states[0]);
         int y = int.Parse(states[1]);
         string directionStringCommand = states[2];
-        _rover._position = new Position(x, y);
-        _rover._direction = DirectionFactory.CreateFrom(directionStringCommand);
+        
+        _rover = new Rover(new Position(x, y), DirectionFactory.CreateFrom(directionStringCommand));
+        
         commands.ToCharArray().ToList().ForEach(c =>
         {
             if (c == 'M')
             {
-                _rover.MoveForward();
+                _rover = _rover.MoveForward();
             }
             else if (c == 'R')
             {
-                _rover.TurnRight();
+                _rover = _rover.TurnRight();
             }
             else if (c == 'L')
             {
-                _rover.TurnLeft();
+                _rover = _rover.TurnLeft();
             }
         });
         return $"{_rover._position.X}:{_rover._position.Y}:{_rover._direction.AsStringCommand()}";
