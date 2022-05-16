839fa46c6f6cb6f8fc9392fb91d44858574715f3
ahmedshabaanhelwa <ahmedshabaan@outlook.com>
2022-05-16 15:03:44 +0200

    General Refactor: Command Pattern: Remove dependency on concrete implementations of commands in switch cases, and use a private field _command of ICommand instead.

--- MarsRover/MarsRover.cs
+++ MarsRover/MarsRover.cs
@@ -2,6 +2,7 @@
 using MarsRover.Rover;
 
 namespace MarsRover;
+
 /// <summary>
 /// A Rover that is capable of exploring Mars!
 /// </summary>
@@ -9,6 +10,7 @@ public class MarsRover
 {
     private readonly string _initialState;
     private Rover.Rover _rover;
+    private ICommand _command;
 
     public MarsRover(string initialState)
     {
@@ -22,20 +24,20 @@ public class MarsRover
         {
             if (c == 'M')
             {
-                var moveForwardCommand = new MoveForwardCommand();
-                _rover = _rover.Apply(moveForwardCommand);
+                _command = new MoveForwardCommand();
             }
             else if (c == 'R')
             {
-                var turnRightCommand = new TurnRightCommand();
-                _rover = _rover.Apply((dynamic)turnRightCommand);
+                _command = new TurnRightCommand();
             }
             else if (c == 'L')
             {
-                var turnLeftCommand = new TurnLeftCommand();
-                _rover = _rover.Apply(turnLeftCommand);
+                _command = new TurnLeftCommand();
             }
+            
+            _rover = _rover.Apply(_command);
         });
+        
         return $"{_rover.Position.X}:{_rover.Position.Y}:{_rover.Direction.AsStringCommand()}";
     }
 }
