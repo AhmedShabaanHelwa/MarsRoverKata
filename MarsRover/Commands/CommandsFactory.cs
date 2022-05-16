80d385cebec77b953d27f41527592d494e884db1
ahmedshabaanhelwa <ahmedshabaan@outlook.com>
2022-05-16 15:24:40 +0200

    General Refactor: Command Pattern: Add comments to CommandsFactory.

--- MarsRover/Commands/CommandsFactory.cs
+++ MarsRover/Commands/CommandsFactory.cs
@@ -1,7 +1,16 @@
 ﻿namespace MarsRover.Commands;
 
+/// <summary>
+/// Commands factory to create the proper concrete implementation of ICommand
+/// </summary>
 internal abstract class CommandsFactory
 {
+    /// <summary>
+    /// Creates the proper concrete implementation of ICommand from a command character.
+    /// </summary>
+    /// <param name="commandCharacter">Command character.</param>
+    /// <returns>Concrete implementation of the command.</returns>
+    /// <exception cref="NotSupportedException">Thrown in case of not supported command.</exception>
     internal static ICommand CreatFromCharacter(char commandCharacter)
     {
         return commandCharacter switch
